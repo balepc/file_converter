94659fe1b606e0aaa203d9734029cbaa5667d605
Valery <valery.visnakov@rails.lv>
2009-03-03 11:15:16 +0200

    Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -72,4 +72,11 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+    :location => '/usr/sbin/sendmail',
+    :arguments      => '-i -t'    
+  }
 end
